cecc2897fce5c27e8aac61907feb499c636ed23f
Becca Penney <70587957+bdpenney@users.noreply.github.com>
2021-03-02 18:21:22 -0330

    JS
    Edited var for let.

--- index.js
+++ index.js
@@ -4,13 +4,15 @@
 let calculateValue = document.getElementById("calculateValue");
 
 calculateValue.addEventListener('click', () => {
-    var num1 = document.getElementById("modValue").value;
-    var num2 = document.getElementById("rollValue").value;
-    var result;
+    let num1 = document.getElementById("modValue").value;
+    let num2 = document.getElementById("rollValue").value;
+    let result;
 
     result = Number(num1) + Number(num2);
     document.getElementById("totalValue").value = result;
 
     return false;
 })
+
+
 
